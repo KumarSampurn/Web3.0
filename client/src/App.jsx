79bca09d321857c04b79cdeb48091507c1427c65
Epsilon <kumarsampurn@outlook.com>
2023-01-06 21:06:30 +0530

    Completed Navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Navbar,Footer,Loader , Services , Welcome, Transaction } from "./components"
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transaction/>
+      <Footer/>
+    </div>
+  )
+}
