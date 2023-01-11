7249f91862a5062a1f9839078db6b0a02a8262ba
Epsilon <kumarsampurn@outlook.com>
2023-01-11 22:41:38 +0530

    Added Service and Transaction page

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -1,6 +1,18 @@
+import React,{useContext} from "react";
+import {TransactionContext} from '../context/TransactionContext';
+import dummyData from "../utils/dummyData"
 const Transaction =() =>{
+    const {currentAccount} = useContext(TransactionContext);
     return(
-        <h1>Transaction</h1>
+        <div className="flex w-full jusitfy-center items-center 2xl:px-20 gradient-bg-transaction">
+            <div className="flex flex-col md:p-12 py-12  px-4 ">
+                {currentAccount ?(
+                    <h3 className="text-white text-3xl text-center my-2">Latest Transaction</h3>
+                ):(
+                    <h3 className="text-white text-3xl text-center my-2">Connect your account to See latest transaction </h3>
+                )}
+            </div>
+        </div>
     )
 }
 
--- client/utils/constants.js
+++
@@ -1,4 +0,0 @@
-
-
-
-export const contractAddress = '0x5FbDB231'
